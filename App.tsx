f324f743177197a083c9928a179874ed1bc7c1de
apppro123 <buchavm@hotmail.com>
2020-11-02 22:29:18 +0100

    started with refreshing at end of day

--- App.tsx
+++ App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { AsyncStorage } from "react-native";
 //library for time/date/...
 import Moment from 'moment';
 //vasern db
@@ -8,7 +9,7 @@ import Main from './src';
 //redux
 import { Provider } from 'react-redux';
 import store from './src/redux/store';
-import {refreshAllLists} from "./src/redux/actions";
+import { refreshAllLists } from "./src/redux/actions";
 
 //format for moment
 const FORMAT = 'DD-MM-YYYY';
@@ -16,7 +17,7 @@ const FORMAT = 'DD-MM-YYYY';
 interface StateI {
   calculationsMade: boolean,
   latestDate: string,
-  intervalId?: NodeJS.Timeout,
+  timeoutId?: NodeJS.Timeout,
   loadedData: boolean
 }
 
@@ -24,77 +25,78 @@ class App extends React.Component<{}, StateI> {
   state = {
     latestDate: Moment().format(FORMAT),
     calculationsMade: false,
-    intervalId: setInterval(() => null, 60000),
+    timeoutId: undefined as undefined | NodeJS.Timeout,
     loadedData: false
   };
 
   componentDidMount() {
+
     //this.makeCalculations();
+
     //first call on db and set to-dos
     VasernDB.onLoaded(() => {
       store.dispatch(refreshAllLists());
-      this.setState({loadedData: true})
+      this.setState({ loadedData: true })
     })
   }
 
   componentWillUnmount() {
-    clearInterval(this.state.intervalId);
+    const { timeoutId } = this.state;
+    if (timeoutId) {
+      clearTimeout(timeoutId);
+    }
   }
 
   /* look if date changed and refresh ToDos if necessary */
-  /* makeCalculations = async () => {
+  /* refresh ^=  refresh all views and set all-day to-dos done to false plus 
+  refresh streaks if necessary (reset currentStreak to 0 if not done that day) */
+  makeCalculations = async () => {
     const LATEST_DATE = 'latestDate';
     try {
       //get last saved date
       let latestDate = await AsyncStorage.getItem(LATEST_DATE);
       if (latestDate !== null) {
         if (!Moment(latestDate, FORMAT).isSame(Moment(), 'day')) {
-          //date has changed =>
-          //update to-dos 
-          const allToDos: ToDoI[] = await updateAfterDateChanged();
-          //update redux
-          store.dispatch(setRefreshAllToDos(allToDos));
+          /* 
+          refresh
+           */
           //update date in AsyncStorage and state
           const newLatestDate = Moment().format(FORMAT);
           await AsyncStorage.setItem(LATEST_DATE, newLatestDate);
-          this.state.latestDate = newLatestDate;
-          this.state.intervalId = setInterval(this.checkDateChanged, 60000);
+          //get milliseconds from now to end of day
+          let diffToDayEnd = Moment().endOf("day").diff(Moment());
+          //call function to refresh at end of day
+          this.state.timeoutId = setTimeout(this.changeToDosNewDay, diffToDayEnd);
         } else {
           //date has not changed
           //get and set to-dos
-          const allToDos = await getAllToDos();
-          store.dispatch(setRefreshAllToDos(allToDos));
-          //set latest date to state and
-          //set interval (every minute) if date has changed
-          this.state.latestDate = latestDate;
-          this.state.intervalId = setInterval(this.checkDateChanged, 60000);
+          store.dispatch(refreshAllLists());
+          //get milliseconds from now to end of day
+          let diffToDayEnd = Moment().endOf("day").diff(Moment());
+          // call function to refresh at end of day
+          this.state.timeoutId = setTimeout(this.changeToDosNewDay, diffToDayEnd);
         }
       } else {
+        //LATEST_DATE not set => first launch of application
         const newLatestDate = Moment().format(FORMAT);
-        this.state.latestDate = newLatestDate;
         await AsyncStorage.setItem(LATEST_DATE, newLatestDate);
-        this.state.intervalId = setInterval(this.checkDateChanged, 60000);
+        //get milliseconds from now to end of day
+        let diffToDayEnd = Moment().endOf("day").diff(Moment());
+        // call function to refresh at end of day
+        this.state.timeoutId = setTimeout(this.changeToDosNewDay, diffToDayEnd);
       }
       this.setState({ calculationsMade: true });
     } catch (error) {
       alert('Sorry, an error occured!');
       this.setState({ calculationsMade: true });
     }
-  }; */
+  };
 
-  /* checkDateChanged = async () => {
-    const LATEST_DATE = 'latestDate';
-    //get latest date from state
-    const latestDate = Moment(this.state.latestDate, FORMAT);
-    if (!latestDate.isSame(Moment(), 'day')) {
-      //date has changed =>
-      //update to-dos and date in AsyncStorage and state
-      await updateAfterDateChanged();
-      const newLatestDate = Moment().format(FORMAT);
-      await AsyncStorage.setItem(LATEST_DATE, newLatestDate);
-      this.state.latestDate = newLatestDate;
-    }
-  }; */
+  changeToDosNewDay = () => {
+    //change allDay done and streaks
+
+    //refresh views
+  }
 
   render() {
     return (
